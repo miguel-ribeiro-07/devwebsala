12e0ff3a9287d046e7683d710e518635979d635f
Miguel Ribeiro <70304654+miguel-ribeiro-07@users.noreply.github.com>
2023-04-12 21:57:10 -0300

    Continuação Login

--- devweb-main/src/pages/Login/index.js
+++ devweb-main/src/pages/Login/index.js
@@ -1,13 +1,11 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import logo from '../../images/Logo.png'
 import Typography from '@mui/material/Typography';
 
 
@@ -40,9 +38,8 @@ export default function Login() {
               alignItems: 'center',
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
+            <img src={logo} sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            </img>
             <Typography component="h1" variant="h5">
               Login
             </Typography>
@@ -52,7 +49,7 @@ export default function Login() {
                 required
                 fullWidth
                 id="email"
-                label="Email Address"
+                label="E-mail"
                 name="email"
                 autoComplete="email"
                 autoFocus
@@ -61,32 +58,18 @@ export default function Login() {
                 margin="normal"
                 required
                 fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
+                name="senha"
+                label="Senha"
+                type="senha"
+                id="senha"
               />
               <Button
-                type="submit"
                 fullWidth
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
               >
-                Sign In
+                Fazer Login
               </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
             </Box>
           </Box>
         </Grid>
