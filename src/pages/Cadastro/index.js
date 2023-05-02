06da093bd20a3760334be6f9ff0b56138001407f
Miguel Ribeiro <70304654+miguel-ribeiro-07@users.noreply.github.com>
2023-05-02 20:14:44 -0300

    Validação CPF

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -7,14 +7,42 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import { ReceiptRounded } from '@mui/icons-material';
 
 const Cadastro = () => {
   const [alignment, setAlignment] = React.useState('adm');
+  const [errorCPF, setErrorCPF] = React.useState(false)
+  const [helpCPF, setHelpCPF] = React.useState("")
+  const [errorCNPJ, setErrorCNPJ] = React.useState(false)
+  const [helpCNPJ, setHelpCNPJ] = React.useState("")
 
+  let [CPF, setCPF] = React.useState("")
+  
   const handleChange = (event, newAlignment) => {
     setAlignment(newAlignment);
   }
+
+  function TestaCPF(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+  if (strCPF == "00000000000") return false;
+
+  for (let i =1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) )  return (setErrorCPF(true), setHelpCPF("Digite um cpf válido"))
+
+  Soma = 0;
+    for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return (setErrorCPF(true), setHelpCPF("Digite um cpf válido"))
   
+    return (setErrorCPF(false), setHelpCPF("")) ;
+  }  
 
   return (
       <Container component="main" maxWidth="xs">
@@ -33,22 +61,27 @@ const Cadastro = () => {
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
+                  error={errorCPF}
                   autoComplete="cpf"
                   name="cpf"
                   fullWidth
                   id="cpf"
                   label="CPF"
                   disabled={alignment === 'client' ? true : false}
+                  onChange={(e) => {setCPF(e.target.value)}}
+                  helperText={helpCPF}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField
+                  error={errorCNPJ}
                   autoComplete="cnpj"
                   name="cnpj"
                   fullWidth
                   id="cnpj"
                   label="CNPJ"
                   disabled={alignment === 'adm' || alignment === 'driver' ? true : false}
+                  helperText={helpCNPJ}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -100,6 +133,7 @@ const Cadastro = () => {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              onClick={() => TestaCPF(CPF)}
             >
               Cadastrar
             </Button>
